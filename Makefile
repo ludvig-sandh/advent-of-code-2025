fde0a1c8e88d9d9b513c5d6639866c589c20a45c
Ludvig Sandh <givdul11@hotmail.se>
2025-12-21 17:02:12 +0100

    Cleaned up day 9

--- Makefile
+++ Makefile
@@ -1,26 +1,35 @@
 # Usage:
 #   make day=3
 #   make run day=3
+#   make debug day=3
+#   make run-debug day=3
 
 CXX := g++
-CXXFLAGS := -O2 -std=c++23
 
-# Default target: build the chosen day
+RELEASE_FLAGS := -O2 -std=c++23
+DEBUG_FLAGS   := -O0 -g -std=c++23 -DDEBUG
+
+# Default target: build the chosen day (release)
 .PHONY: all
 all: build
 
 # Normalize the day number to two digits
 DAY := $(shell printf "%02d" $(day))
 
-# Name of the executable
-TARGET := day$(day)
+# Executable names
+TARGET        := day$(day)
 
 build:
-	$(CXX) $(CXXFLAGS) src/day$(DAY).cpp src/utils.cpp -I include -o $(TARGET)
+	$(CXX) $(RELEASE_FLAGS) src/day$(DAY).cpp src/utils.cpp -I include -o $(TARGET)
 
 run: build
 	./$(TARGET)
 
-# Optional: clean
+debug:
+	$(CXX) $(DEBUG_FLAGS) src/day$(DAY).cpp src/utils.cpp -I include -o $(TARGET)
+
+run-debug: debug
+	./$(TARGET)
+
 clean:
 	rm -f day{1..25}
--- src/day09.cpp
+++ src/day09.cpp
@@ -2,74 +2,36 @@
 
 #include <string>
 #include <print>
-#include <set>
 
 struct Point {
     long long x, y;
-    bool operator<(const Point& other) const {
-        return (x == other.x)
-            ? y < other.y
-            : x < other.x;
-    }
 };
 
 using PointPair = std::pair<Point, Point>;
 
-long long ComputeArea(const PointPair& pp) {
-    const auto& [p1, p2] = pp;
+long long ComputeArea(const PointPair& rect) {
+    const auto& [p1, p2] = rect;
     return (std::abs(p2.x - p1.x) + 1) * (std::abs(p2.y - p1.y) + 1);
 }
 
-long long ComputeAreaWith1Border(const PointPair& pp) {
-    const auto& [p1, p2] = pp;
-    return (std::abs(p2.x - p1.x) + 3) * (std::abs(p2.y - p1.y) + 3);
-}
-
-std::vector<PointPair> PairUpPoints(const auto& points) {
+std::vector<PointPair> PairUpAndSortByArea(const auto& points) {
     // Pair up all points
     std::vector<PointPair> pointPairs;
     for (auto it = points.begin(); it != points.end(); ++it) {
-        const auto& pointA = *it;
-        std::for_each(it + 1, points.end(), [&pointPairs, &pointA](const auto& pointB) {
-            pointPairs.emplace_back(pointA, pointB);
-        });
+        for (auto jt = std::next(it); jt != points.end(); ++jt) {
+            pointPairs.emplace_back(*it, *jt);
+        }
     }
+
+    // Sort rectangles by largest area
+    std::sort(pointPairs.begin(), pointPairs.end(), [](const auto& rect1, const auto& rect2) {
+        return ComputeArea(rect1) > ComputeArea(rect2);
+    });
 
     return pointPairs;
 }
 
-template <int offsetX, int offsetY>
-Point GetPointOffset(const Point& p) {
-    return {p.x + offsetX, p.y + offsetY};
-}
-
-bool IsInsidePolygon(const std::vector<Point>& points, const Point p) {
-    int numCrossings = 0;
-    for (int i = 0; i < std::ssize(points); ++i) {
-        int j = (i + 1) % std::ssize(points);
-        const auto& red1 = points[i];
-        const auto& red2 = points[j];
-
-        if (red1.x != red2.x) {
-            continue;
-        }
-        int lineX = red1.x;
-        if (lineX < p.x) {
-            continue;
-        }
-
-        auto [redTop, redBottom] = std::minmax(red1.y, red2.y);
-        if (p.y < redTop || p.y > redBottom) {
-            continue;
-        }
-
-        numCrossings++;
-    }
-
-    return numCrossings % 2 == 1;
-}
-
-bool Do2LinesIntersect(const PointPair& line1, const PointPair& line2) {
+bool DoLinesIntersect(const PointPair& line1, const PointPair& line2) {
     auto [min1X, max1X] = std::minmax(line1.first.x, line1.second.x);
     auto [min2X, max2X] = std::minmax(line2.first.x, line2.second.x);
     auto [min1Y, max1Y] = std::minmax(line1.first.y, line1.second.y);
@@ -85,32 +47,11 @@ bool Do2LinesIntersect(const PointPair& line1, const PointPair& line2) {
 
 bool DoesLineIntersectInputShape(const PointPair& line, const std::vector<Point>& points) {
     for (int i = 0; i < std::ssize(points); ++i) {
-        int j = (i + 1) % std::ssize(points);
-        if (Do2LinesIntersect(line, {points[i], points[j]})) {
+        int j = (i == std::ssize(points) - 1) ? 0 : i + 1; // j = (i + 1) mod #points
+        if (DoLinesIntersect(line, {points[i], points[j]})) {
             return true;
         }
     }
-    return false;
-}
-
-bool CheckCandidate(const PointPair& DAPair, const std::set<Point>& pointsSet) {
-    auto [minX, maxX] = std::minmax(DAPair.first.x, DAPair.second.x);
-    auto [minY, maxY] = std::minmax(DAPair.first.y, DAPair.second.y);
-    
-    const Point topLeft{minX - 1, minY - 1};
-    const Point bottomRight{maxX + 1, maxY + 1};
-    if (pointsSet.find(topLeft) != pointsSet.end() &&
-        pointsSet.find(bottomRight) != pointsSet.end()) {
-        return true;
-    }
-    
-    const Point topRight{maxX + 1, minY - 1};
-    const Point bottomLeft{minX - 1, maxY + 1};
-    if (pointsSet.find(topRight) != pointsSet.end() &&
-        pointsSet.find(bottomLeft) != pointsSet.end()) {
-        return true;
-    }
-
     return false;
 }
 
@@ -127,54 +68,33 @@ int main() {
         return Point{x, y};
     });
 
-    std::set<Point> pointsSet;
-    pointsSet.insert(points.begin(), points.end());
+    auto rectangles = PairUpAndSortByArea(points);
 
-    auto pointPairs = PairUpPoints(points);
-
-    // Sort rectangles by largest area
-    std::sort(pointPairs.begin(), pointPairs.end(), [](const auto& pp1, const auto& pp2) {
-        return ComputeArea(pp1) > ComputeArea(pp2);
-    });
-
-    long long answerA = ComputeArea(pointPairs.front());
-
+    // Since rectangles are ordered by area (decreasing), pick the first one
+    long long answerA = ComputeArea(rectangles.front());
     Util::ProvideSolution(answerA, Util::Part::A);
-    
-    // Find all diagonally adjacent points
-    std::vector<Point> DAPoints;
-    std::transform(points.begin(), points.end(), std::back_inserter(DAPoints), GetPointOffset<1, 1>);
-    std::transform(points.begin(), points.end(), std::back_inserter(DAPoints), GetPointOffset<-1, 1>);
-    std::transform(points.begin(), points.end(), std::back_inserter(DAPoints), GetPointOffset<-1, -1>);
-    std::transform(points.begin(), points.end(), std::back_inserter(DAPoints), GetPointOffset<1, -1>);
 
-    std::erase_if(DAPoints, [&points](const auto& point) {
-        return !IsInsidePolygon(points, point);
-    });
+    for (const auto& rectangle : rectangles) {
+        // Idea: Shrink rectangle by 1. If this rectangle doesn't intersect with any edge of the input shape,
+        // expanding it again will be safe (meaning it will only contain red/green tiles).
+        const auto& [p1, p2] = rectangle;
+        auto [minX, maxX] = std::minmax(p1.x, p2.x);
+        auto [minY, maxY] = std::minmax(p1.y, p2.y);
 
-    auto DAPairs = PairUpPoints(DAPoints);
+        // It's enough to only check the rectangle borders (4 lines)
+        PointPair horizLine1{{minX + 1, minY + 1}, {maxX - 1, minY + 1}};
+        PointPair horizLine2{{minX + 1, maxY - 1}, {maxX - 1, maxY - 1}};
+        PointPair vertiLine1{{minX + 1, minY + 1}, {minX + 1, maxY - 1}};
+        PointPair vertiLine2{{maxX - 1, minY + 1}, {maxX - 1, maxY - 1}};
 
-    // Sort rectangles by largest area with border
-    std::sort(DAPairs.begin(), DAPairs.end(), [](const auto& pp1, const auto& pp2) {
-        return ComputeAreaWith1Border(pp1) > ComputeAreaWith1Border(pp2);
-    });
+        if (DoesLineIntersectInputShape(horizLine1, points)) continue;
+        if (DoesLineIntersectInputShape(horizLine2, points)) continue;
+        if (DoesLineIntersectInputShape(vertiLine1, points)) continue;
+        if (DoesLineIntersectInputShape(vertiLine2, points)) continue;
 
-    for (const auto& DAPair : DAPairs) {
-        // If this rectangle doesn't intersect with any edge, we can add a border of 1 around it and still fit
-        const auto& [p1, p2] = DAPair;
-        PointPair horizLine1{p1, {p2.x, p1.y}};
-        PointPair horizLine2{p2, {p1.x, p2.y}};
-        PointPair vertiLine1{p1, {p1.x, p2.y}};
-        PointPair vertiLine2{p2, {p2.x, p1.y}};
-        if (!DoesLineIntersectInputShape(horizLine1, points) &&
-            !DoesLineIntersectInputShape(horizLine2, points) &&
-            !DoesLineIntersectInputShape(vertiLine1, points) &&
-            !DoesLineIntersectInputShape(vertiLine2, points)) {
-            if (CheckCandidate(DAPair, pointsSet)) {
-                long long answerB = ComputeAreaWith1Border(DAPair);
-                Util::ProvideSolution(answerB, Util::Part::B);
-                break;
-            }
-        }
+        // Successful candidate. Pick first and break since rectangles are ordered by area
+        long long answerB = ComputeArea(rectangle);
+        Util::ProvideSolution(answerB, Util::Part::B);
+        break;
     }
 }
